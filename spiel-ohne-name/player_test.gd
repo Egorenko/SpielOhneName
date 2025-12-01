4b52e3d742af9f0a95eb0070394faa894d3e8451
Julius Knust <julius.knust@gmail.com>
2025-12-01 23:03:04 +0100

    try to make shape of attack local; changed most lines to arrays(consistency); added match(switch case)

--- spiel-ohne-name/player_test.gd
+++ spiel-ohne-name/player_test.gd
@@ -20,8 +20,8 @@ var atk_vertces_count_min:int = 3 #at least n vertces in 'atk_vertces'
 var atk_vertces_count_max:int = 30#at most n vertces in 'atk_vertces'
 var atk_noise:float = 2.5#max distance a vertex can diviate from start-end-line
 var atk_ex_noise:float = 0.6#percentage of equality between start/end (1 -> perfect in middle | 0.0 -> distance to sstart/end irrelevant)
-var atk_ex_maxdis:float = 50#max distance most farthest away vertex is allowed to have
-var atk_ex_mindis:float = 10#min distance most closest vertex needs to have (seperate from atk_noise)
+var atk_ex_maxDis:float = 50#max distance most farthest away vertex is allowed to have
+var atk_ex_minDis:float = 10#min distance most closest vertex needs to have (seperate from atk_noise)
 
 # |PI| left ; 0 right ; PI/2 down ; -PI/2 up
 # [3 .. -3]
@@ -38,7 +38,9 @@ func _physics_process(delta: float) -> void:
 		print("STOP")
 		if atk_vertces_count_min <= atk_vertces.size():#try process
 			print("WORK EARLY")
-			processShape(atk_vertces, atk_noise, atk_ex_noise)
+			$atk_area/atk_view.visible = false
+			var i:int = processShape(atk_vertces, atk_noise, atk_ex_noise)
+			process_attack($Sprite2D_test.global_position, i, atk_vertces)
 		else :
 			print("CLEAR")
 		atk_vertces.clear()
@@ -49,14 +51,16 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_pressed("LMB") and check:#while hold
 		test_tick-= 1#count timer
 		if(test_tick >= 0):#if timer not end
-			atk_vertces.append(get_global_mouse_position())#collect vertex
-			$atk_area/atk_ray.add_point(get_local_mouse_position())
+			atk_vertces.append(to_local(get_global_mouse_position()))#collect vertex
+			$atk_area/atk_ray.add_point(to_local(get_global_mouse_position()))
 
 		#too many -> end?
 		if atk_vertces.size() >= atk_vertces_count_max:#force end
 			print("WORK LATE")
 			check = false
-			processShape(atk_vertces, atk_noise, atk_ex_noise)
+			$atk_area/atk_view.visible = false
+			var i:int = processShape(atk_vertces, atk_noise, atk_ex_noise)
+			process_attack($Sprite2D_test.global_position, i, atk_vertces)
 
 # Get the input direction and handle the movement/deceleration.
 # As good practice, you should replace UI actions with custom gameplay actions.
@@ -102,102 +106,108 @@ func _physics_process(delta: float) -> void:
 
 #-------------------------------------------------------------------------------
 # finds what shape array could make
-func processShape(array:Array[Vector2], line_noise:float, ex_noise:float) -> int:
-	var start:Vector2 = array.front()
-	var end:Vector2 = array.back()
+func processShape(line:Array[Vector2], line_noise:float, ex_noise:float) -> int:
+	var start:Vector2 = line.front()
+	var end:Vector2 = line.back()
 	var y2y1:float = (end[1]-start[1])
 	var x2x1:float = (end[0]-start[0])
-	#visual help ->
+	
+#visual help ->
 	var correctLine = Line2D.new()
-	correctLine.points = array
+	correctLine.points = line
 	correctLine.width = 1.0
-	#<- visual help
-	var maxDisVP:Vector3#safe vertex AND its distance to line
-	var maxDisVN:Vector3#safe vertex AND its distance to line
-	var SE_dis:float = pow(pow(y2y1, 2) + pow(x2x1, 2), 1.0/2.0)#distance of start-end-line
-	#test nois per vertex ->
-	for vertex in array:
+#<- visual help
+
+	var maxDis_vector_positive:Vector3#safe vertex[0][1] AND its distance to line[2]
+	var maxDis_vector_negative:Vector3
+	
+	var start_end_dis:float = pow(pow(y2y1, 2) + pow(x2x1, 2), 1.0/2.0)#distance of start-end-line
+	var maxDis:float = 0.0
+	var maxDisV:Vector2 = Vector2(0.0, 0.0)
+	
+#test nois per vertex ->
+	for vertex in line:
 		var dis:float = (
 			(y2y1 * vertex[0] - x2x1*vertex[1] + (end[0] * start[1]) - (end[1] * start[0]))
-			/SE_dis
+			/start_end_dis
 			)
-		if(dis >= maxDisVP[2]):
-			maxDisVP[0] = vertex[0]
-			maxDisVP[1] = vertex[1]
-			maxDisVP[2] = dis
-		if(dis <= maxDisVN[2]):
-			maxDisVN[0] = vertex[0]
-			maxDisVN[1] = vertex[1]
-			maxDisVN[2] = dis
-	#<- test nois per vertex
-	var maxDis:float = 0.0
-	var maxDisV:Vector2 = $Sprite2D_test.global_position
-	#find vertex and distance with max distance to line
-	if  abs(maxDisVN[2]) < abs(maxDisVP[2]):
-		maxDis = abs(maxDisVP[2])
-		maxDisV = Vector2(maxDisVP[0],maxDisVP[1])
+		if(dis >= maxDis_vector_positive[2]):#safe most farthest away
+			maxDis_vector_positive[0] = vertex[0]
+			maxDis_vector_positive[1] = vertex[1]
+			maxDis_vector_positive[2] = dis
+		if(dis <= maxDis_vector_negative[2]):#same but negative
+			maxDis_vector_negative[0] = vertex[0]
+			maxDis_vector_negative[1] = vertex[1]
+			maxDis_vector_negative[2] = dis
+#<- test nois per vertex
+
+	if  abs(maxDis_vector_negative[2]) < abs(maxDis_vector_positive[2]):
+		maxDis = abs(maxDis_vector_positive[2])
+		maxDisV = Vector2(maxDis_vector_positive[0],maxDis_vector_positive[1])
 	else:
-		maxDis = abs(maxDisVN[2])
-		maxDisV = Vector2(maxDisVN[0],maxDisVN[1])
+		maxDis = abs(maxDis_vector_negative[2])
+		maxDisV = Vector2(maxDis_vector_negative[0],maxDis_vector_negative[1])
+
 #no vertces too far away -> should be line
-	if maxDisVP[2] < line_noise and abs(maxDisVN[2]) < line_noise :
+	if maxDis_vector_positive[2] < line_noise and abs(maxDis_vector_negative[2]) < line_noise :
 		correctLine.default_color = Color(0.0,0.0,255.0,255.0)
 		get_parent().add_child(correctLine)
 		print("Line")
-		process_attack($Sprite2D_test.global_position, 0, correctLine)
 		return 0#0 = straight
+
 #further away then allowed
-	if maxDis > atk_ex_maxdis:
+	if maxDis > atk_ex_maxDis:
 		print("TOO FAR AWAY")
 		return -1
 
-	if maxDisVP[2] >= line_noise and abs(maxDisVN[2]) >= line_noise :#if extrema in two directions -> neither line nore arch
+	if maxDis_vector_positive[2] >= line_noise and abs(maxDis_vector_negative[2]) >= line_noise :#if extrema in two directions -> neither line nore arch
 		print("EXTREME IN POS AND NEG")
 		return -1#polygon
 
 	var StoEX:float = abs(start.distance_to(maxDisV))#length |start->extrema|
-	#print("StoEX", StoEX)
 	var EtoEX:float = abs(end.distance_to(maxDisV))#length |end->extrema|
-	#print("EtoEX", EtoEX)
 	if (EtoEX * ex_noise <= StoEX and StoEX <= EtoEX) or (StoEX * ex_noise <= EtoEX and EtoEX <= StoEX) :#one side between o.side and acceptable length o.side 
 		print("IN MIDDLE")
 	else : 
 		print("NOT MIDDLE")
 		return -1
+
 #if one* vertex too far away -> line has to bend -> not (straight) line
-	if maxDis >= line_noise and maxDis >= atk_ex_mindis:
+	if maxDis >= line_noise and maxDis >= atk_ex_minDis:
 		correctLine.default_color = Color(0.0,255.0,0.0,255.0)
 		get_parent().add_child(correctLine)
 		print("ARCH")
-		process_attack($Sprite2D_test.global_position, 1, correctLine)
 		return 1#1 = NOT straight
 	print("DID NOT FIND SHAPE")
 	return -1
 
 #-------------------------------------------------------------------------------
 # tryes to find fitting attack for shape
-func process_attack(startPos:Vector2, atk_type:int, atk_shape:Line2D)-> void:
-#reset Sprite
-	$atk_area/atk_view.rotation = 0
-	
-	var midLine:Vector2 = atk_shape.get_point_position(0) -  atk_shape.get_point_position(atk_shape.get_point_count()-1)#direct line from start to end -> middle line
-	var mid:Vector2 = atk_shape.get_point_position(atk_shape.get_point_count()-1) + midLine*0.5#middle of attackline
-	var midLine_rotation:float = atk_shape.get_point_position(0).angle_to_point(atk_shape.get_point_position(atk_shape.get_point_count()-1))
+func process_attack(startPos:Vector2, atk_type:int, atk_shape:Array[Vector2])-> void:
+	$atk_area/atk_view.visible = true
+	$atk_area/atk_view.rotation = 0#reset Sprite
+	var midLine:Vector2 = atk_shape[0] -  atk_shape[atk_shape.size()-1]#direct line from start to end -> middle line
+	var mid:Vector2 = atk_shape[atk_shape.size()-1] + midLine*0.5#middle of attackline
+	var midLine_rotation:float = atk_shape[0].angle_to_point(atk_shape[atk_shape.size()-1])
 	var char_to_mid:Vector2 = mid - startPos#vector for angle player to middle of attackline
+	var angle_char_to_Line:float = abs(char_to_mid.angle_to(midLine))
 
 #line
-	if(atk_type == 0):
-		if (PI * 0.5 - rotation_noise <= abs(char_to_mid.angle_to(midLine))) and (abs(char_to_mid.angle_to(midLine)) <= PI * 0.5 + rotation_noise):
-			move_rotate_sprite($atk_area/atk_view, $Sprite2D_test.global_position, offset, $Sprite2D_test.get_angle_to(mid), PI * -0.5)
-			$atk_area/atk_view.texture = load("res://assets/20251126test_shield_smallF.png")#upper end to player
-		else:
-			move_rotate_sprite($atk_area/atk_view, $Sprite2D_test.global_position, offset, midLine_rotation, PI * 0.25)
-			$atk_area/atk_view.texture = load("res://assets/20251126test_swort_small+diagonal.png")#handle to player
-
-#arch
-	if(atk_type == 1):
-		move_rotate_sprite($atk_area/atk_view, $Sprite2D_test.global_position, offset, $Sprite2D_test.get_angle_to(mid), PI * 0.25)
-		$atk_area/atk_view.texture = load("res://assets/20251126test_wand_nature_blue_small+diagonal.png")
+	match atk_type:
+		0:#line
+			if (PI * 0.5 - rotation_noise <= angle_char_to_Line) and (angle_char_to_Line <= PI * 0.5 + rotation_noise):
+				move_rotate_sprite($atk_area/atk_view, $Sprite2D_test.global_position, offset, $Sprite2D_test.get_angle_to(mid), PI * -0.5)
+				$atk_area/atk_view.texture = load("res://assets/20251126test_shield_smallF.png")#upper end to player
+			else:
+				move_rotate_sprite($atk_area/atk_view, $Sprite2D_test.global_position, offset, midLine_rotation, PI * 0.25)
+				$atk_area/atk_view.texture = load("res://assets/20251126test_swort_small+diagonal.png")#handle to player
+			return
+		1:#arch
+			move_rotate_sprite($atk_area/atk_view, $Sprite2D_test.global_position, offset, $Sprite2D_test.get_angle_to(mid), PI * 0.25)
+			$atk_area/atk_view.texture = load("res://assets/20251126test_wand_nature_blue_small+diagonal.png")
+			return
+	print("NOT FOUND")
+	$atk_area/atk_view.visible = false
 	return
 
 #-------------------------------------------------------------------------------
