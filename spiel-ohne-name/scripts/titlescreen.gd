c6a3c761dfd22d4aa894a73b2687dafa888b78bc
C0nstantinHerrmann <cherrmannmd@gmail.com>
2026-01-15 15:57:32 +0100

    Added a tutorial button in the titlescreen and made a rough tutorial scene. removed .import files

--- spiel-ohne-name/scripts/titlescreen.gd
+++ spiel-ohne-name/scripts/titlescreen.gd
@@ -7,3 +7,7 @@ func _on_button_button_down() -> void:
 
 func _on_credit_button_button_down() -> void:
 	get_tree().change_scene_to_file("res://scenes/credits.tscn");
+
+
+func _on_tutorial_button_button_down() -> void:
+	get_tree().change_scene_to_file("res://scenes/tutorial.tscn");
